981a0163883a604f19a3870fd610b3753a9d91fc
Mohammad Abbas <mohammad.abbas86@gmail.com>
2012-09-03 21:07:13 +0100

    added database settings

--- YouSingDotCom/settings.py
+++ YouSingDotCom/settings.py
@@ -11,12 +11,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'YouSingDotCom',                      # Or path to database file if using sqlite3.
+        'USER': 'yousingdotcomapp',                      # Not used with sqlite3.
+        'PASSWORD': 'yousingdotcomapp',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
