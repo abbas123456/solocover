fb3ed9b868c1931570c5ad13299f3e91026c2abc
Mohammad Abbas <mohammad.abbas86@gmail.com>
2012-10-10 00:28:24 +0100

    Must now enter current password when changing password on edit profile page (User update form)

--- account/forms.py
+++ account/forms.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 from account.models import UserProfile
 from django.core.exceptions import ValidationError
 from django.db.models.fields.files import FieldFile
+from django.contrib.auth import models
 
 class UserProfileForm(ModelForm):
     accepted_file_types = ['image/jpeg']
-    
     class Meta:
         model = UserProfile
         exclude = ('user')
@@ -20,6 +20,7 @@ class UserProfileForm(ModelForm):
         return file_object
 
 class UserUpdateForm(ModelForm):
+    current_password = forms.CharField(max_length=200,widget=forms.PasswordInput(render_value=True))
     new_password = forms.CharField(max_length=200,widget=forms.PasswordInput(render_value=True))
     new_password_confirm = forms.CharField(max_length=200,widget=forms.PasswordInput(render_value=True))
     
@@ -30,19 +31,24 @@ class UserUpdateForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(UserUpdateForm, self).__init__(*args, **kwargs)
         self.fields['email'].required = True
+        self.fields['current_password'].required = False
         self.fields['new_password'].required = False
         self.fields['new_password_confirm'].required = False
         
     def clean(self):
         cleaned_data = super(UserUpdateForm, self).clean()
-        if 'new_password' not in cleaned_data and 'new_password_confirm' not in cleaned_data:
+        if 'new_password' not in cleaned_data and 'new_password_confirm' not in cleaned_data and 'current_password' not in cleaned_data:
             return cleaned_data
         
+        current_password = cleaned_data['current_password']
         new_password = cleaned_data['new_password']
         new_password_confirm = cleaned_data['new_password_confirm']
-        
+        if not models.check_password(current_password, self.instance.password):
+            raise ValidationError("Current password is incorrect")
+        if not new_password or not new_password_confirm:
+            raise ValidationError("Please enter and confirm a new password")
         if new_password != new_password_confirm:
-            raise ValidationError("Passwords do not match")
+            raise ValidationError("New passwords do not match")
         
         return cleaned_data
     
