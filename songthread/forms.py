782bcad23e037316fcb99cbc4aa32e778cdb5e25
Mohammad Abbas <mohammad.abbas86@gmail.com>
2012-10-20 00:01:14 +0100

    Removed maximum length requirement for mp3

--- songthread/forms.py
+++ songthread/forms.py
@@ -1,6 +1,3 @@
-from mutagen.mp3 import MP3
-
-from math import ceil
 from django.forms import ModelForm
 from django.core.exceptions import ValidationError
 from songthread.models import Song, Songthread, Comment
@@ -16,16 +13,8 @@ class SongForm(ModelForm):
     accepted_file_types = ['audio/mpeg']
     def clean_file(self):
         file_object = self.cleaned_data['file']
-        file_path = file_object.temporary_file_path()
         if file_object.content_type not in self.accepted_file_types:
             raise ValidationError("Your song needs to be in mp3 format")
-        
-        if file_object.content_type =='audio/mpeg':            
-            mp3_file = MP3(file_path)
-            number_of_seconds = ceil(mp3_file.info.length)
-             
-        if number_of_seconds > 180:
-            raise ValidationError("Your song needs to be three minutes or less")
         else:
             return file_object
     class Meta:
