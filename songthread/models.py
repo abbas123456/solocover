56664f8834295b0ed280e52bc06be77029c70505
Mohammad Abbas <mohammad.abbas86@gmail.com>
2012-10-21 19:26:43 +0100

    Songthreads now have slugified urls

--- songthread/models.py
+++ songthread/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 
 from music.models import Track
 
@@ -7,10 +8,23 @@ class Songthread(models.Model):
     track = models.OneToOneField(Track)
     user = models.ForeignKey(User)
     created_date = models.DateTimeField()
+    slug = models.SlugField()
     
     def __unicode__(self):
         return self.track.name
     
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.track.name)
+        return super(Songthread, self).save(*args, **kwargs)
+    
+    @models.permalink
+    def get_absolute_url(self):
+        return ('songthread_detail', (), {
+            'slug': self.slug,
+            'pk': self.id,
+        })
+
 class Song(models.Model):
     songthread = models.ForeignKey(Songthread)
     user = models.ForeignKey(User)
